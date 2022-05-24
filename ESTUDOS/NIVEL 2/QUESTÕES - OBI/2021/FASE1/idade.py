17ededcc9f4ef6aefdc14dee51a71c33ca7f0eee
evenilsonlp <evenilsonlp@gmail.com>
2022-05-23 22:14:06 -0300

    adicionando resolução de "Idade de Camila"

---
+++ ESTUDOS/NIVEL 2/QUESTÕES - OBI/2021/FASE1/idade.py
@@ -0,0 +1,7 @@
+idade1 = input()
+idade2 = input()
+idade3 = input()
+
+# if(((idade1 >= idade2) and (idade1 <= idade3)) or ((idade1 <= idade2) and (idade1 >= idade3))): print(idade1) 
+# elif(((idade2 >= idade1) and (idade2 <= idade3)) or ((idade2 <= idade1) and (idade2 >= idade3))): print(idade2)
+# else: print(idade3)
